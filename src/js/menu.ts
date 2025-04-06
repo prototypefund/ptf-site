93fadd2962059188c8b0ffff192caea1c85d9671
Sev Furneaux <sevfurneaux@gmail.com>
2025-04-06 15:12:46 +0100

    Add menu interaction

--- src/js/menu.ts
+++ src/js/menu.ts
@@ -1,9 +1,31 @@
 export default function initMenu() {
   const header = document.querySelector("#js-header");
+  const menu = document.querySelector("#js-menu-sandwich");
 
-  if (!header) {
+  if (!header || !menu) {
     return;
   }
+
+  menu.addEventListener("click", () => {
+    const menuOpening = !header.classList.contains("menu-open");
+    menuOpening ? lockPageScroll() : unlockPageScroll();
+    header.classList.toggle("menu-open");
+  });
+
+  document.onkeydown = function (event) {
+    if ("key" in event && (event.key === "Escape" || event.key === "Esc")) {
+      header.classList.remove("menu-open");
+      unlockPageScroll();
+    }
+  };
+
+  const lockPageScroll = () => {
+    document.documentElement.classList.add("overflow-hidden");
+  };
+
+  const unlockPageScroll = () => {
+    document.documentElement.classList.remove("overflow-hidden");
+  };
 
   const menus = header.querySelectorAll(".js-menu");
 
