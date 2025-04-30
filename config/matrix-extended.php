4e231352bd03ff3a760013a8f52773bc67b0a8ef
Sev Furneaux <sevfurneaux@gmail.com>
2025-04-30 14:31:32 +0100

    Add timeline module

--- config/matrix-extended.php
+++ config/matrix-extended.php
@@ -11,7 +11,7 @@
               'groups' => [
                 [
                   'label' => \Craft::t('app', 'Blocks'),
-                  'types' => ['sideHeadingText', 'relatedLinkList', 'expandableText', 'relatedPublications', 'peopleGrid', 'steps', 'relatedCtaBanner', 'embed', 'newsletter'],
+                  'types' => ['sideHeadingText', 'relatedLinkList', 'expandableText', 'relatedTimeline', 'relatedPublications', 'peopleGrid', 'steps', 'relatedCtaBanner', 'embed', 'newsletter'],
                 ],
                 [
                   'label' => \Craft::t('app', 'Teasers'),
