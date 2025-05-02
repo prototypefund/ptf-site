e7f271c16d96116d2d9aef9120d7e5420564ca15
Sev Furneaux <sevfurneaux@gmail.com>
2025-05-02 11:59:47 +0100

    [feature] add chart partial (#122)

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,12 @@
 import "../css/main.css";
+import { BarChartTest } from "./components/bar-chart";
 import { DisplayToggle } from "./components/display-toggle";
 import { MainNav } from "./components/main-nav";
 
 if (!customElements.get("display-toggle") || customElements.get("main-nav")) {
   customElements.define("display-toggle", DisplayToggle);
   customElements.define("main-nav", MainNav);
+  customElements.define("bar-chart", BarChartTest);
 }
 
 window.addEventListener("beforeprint", () => {
