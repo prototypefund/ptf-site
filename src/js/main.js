b22ff2eb2f98c4674b6424622ff4f42802db1115
Sev Furneaux <sevfurneaux@gmail.com>
2025-05-15 16:10:18 +0100

    [chore] add homepage header text animation (#183)

--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,18 @@ import "../css/main.css";
 import { BarChartTest } from "./components/bar-chart";
 import { DisplayToggle } from "./components/display-toggle";
 import { MainNav } from "./components/main-nav";
+import { TextAnimation } from "./components/text-animation";
 
-if (!customElements.get("display-toggle") || customElements.get("main-nav")) {
+if (
+  !customElements.get("display-toggle") ||
+  customElements.get("main-nav") ||
+  customElements.get("bar-chart") ||
+  customElements.get("text-animation")
+) {
   customElements.define("display-toggle", DisplayToggle);
   customElements.define("main-nav", MainNav);
   customElements.define("bar-chart", BarChartTest);
+  customElements.define("text-animation", TextAnimation);
 }
 
 window.addEventListener("beforeprint", () => {
