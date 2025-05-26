76c5344a2f3dfb3f349797c36acbf454930227ff
Sev Furneaux <sevfurneaux@gmail.com>
2025-05-26 10:39:13 +0100

    [chore] add knock knock for production

--- config/knock-knock.php
+++ config/knock-knock.php
@@ -7,6 +7,11 @@ return [
     'enabled' => false,
     'enableCpProtection' => false
   ],
+  'production' => [
+    'enabled' => App::env('KNOCK_KNOCK_ENABLED'),
+    'enableCpProtection' => true,
+    'password' => App::env('KNOCK_KNOCK_PASSWORD'),
+  ],
   'staging' => [
     'enabled' => App::env('KNOCK_KNOCK_ENABLED'),
     'enableCpProtection' => true,
